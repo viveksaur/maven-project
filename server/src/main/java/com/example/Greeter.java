a3528fe50750a6f5c7fb5834a67dc4df2d6e7e6c
viveksaur <40718204+viveksaur@users.noreply.github.com>
2020-04-12 16:10:56 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,4 +1,4 @@
-package com.example;
+inpackage com.example;
 
 /**
  * This is a class.
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
+    return String.format("Hello, %s!", jenkins);
   }
 }
